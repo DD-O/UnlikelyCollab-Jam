191ba1beeb61b35b90dc5a708e727585bd78d172
jamjuneru <pksay@proton.me>
2025-03-03 13:14:32 -0800

    Added Group Perception Box into Final Level

--- UC-Jam/Assets/Scripts/Activation/FollowerActivator.cs
+++ UC-Jam/Assets/Scripts/Activation/FollowerActivator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FollowerActivator : MonoBehaviour
 {
     public GameObject follower; // Assign the object to activate in the Inspector
+    public GroupBox groupBox; // JJ Script Reference to Add Follower to the Group Perceptive Box
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,6 +13,10 @@ public class FollowerActivator : MonoBehaviour
             {
                 follower.SetActive(true); // Activate the follower
                 MusicManager.Instance.TriggerNextSong();
+                groupBox.followerGroup.Add(follower); // JJ Add Follower to Group Box
+                if (groupBox.followerGroup.Count > 1) { // JJ If there's more than just Square in the Group...
+                    groupBox.padding = 6; // JJ ...Then Give the Group Perception Box Padding!
+                }
 
             }
             Destroy(gameObject); // Destroy this object
